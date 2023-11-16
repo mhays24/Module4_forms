291b53c57e1dd0deb3d08fb4779a360698426b4e
mhays24 <mhays24@basecampcodingacademy.org>
2023-11-16 11:07:07 -0600

    updated

--- app/views.py
+++ app/views.py
@@ -1,33 +1,36 @@
 from django.shortcuts import render
-from app.forms import AddForm, HeyYouForm
+from app.forms import HeyYouForm, AgeInForm, OrderTotalForm
 from django.http import HttpResponse
 
-
-def root(request):
-    print(request.POST)
-    return render(request, "root.html")
-
-
-def add(request):
-    form = AddForm(request.GET)
-    if form.is_valid():
-        x = form.cleaned_data["x"]
-        y = form.cleaned_data["y"]
-        answer = x + y
-        return render(
-            request, "add.html", {"form": form, "x": x, "y": y, "answer": answer}
-        )
-    else:
-        return render(request, "add.html", {"form"})
-
-
 def hey_you(request):
-    name = HeyYouForm(request.GET)
-    if request.method == 'POST':
-        form = HeyYouForm(request.POST)
-        form.is_valid()
-        name = form.cleaned_data['name']
+    form = HeyYouForm(request.GET)
+    if form.is_valid():
+        name = form.cleaned_data["name"]
+        return render(request, "hey_you.html", {"form": form, "name": name})
     else:
         form = HeyYouForm()
+        return render(request, "hey_you.html", {"form": form})
 
-    return render(request, 'hey_you.html', {'form': form, 'name': name})
+def age_in(request):
+    form = AgeInForm(request.GET)
+    if form.is_valid():
+        year = form.cleaned_data["year"]
+        BYear = form.cleaned_data["BYear"]
+        age = int(year) - int(BYear)
+        return render(request, "age_in.html", {"form": form, "age": age, "year": year, "BYear": BYear})
+    else:
+        form = AgeInForm()
+        return render(request, "age_in.html", {"form": form})
+    
+
+def order_total(request):
+    form = OrderTotalForm(request.GET)
+    if form.is_valid():
+        burgers = form.cleaned_data["burgers"]
+        fries = form.cleaned_data["fries"]
+        drinks = form.cleaned_data["drinks"]
+        total = (burgers * 4.50) + (fries * 1.50) + (drinks * 1.00)
+        return render(request, "order_total.html", {"form": form, "total": total, "burgers": burgers, "fries": fries, "drinks": drinks})
+    else:
+        form = OrderTotalForm()
+        return render(request, "order_total.html", {"form": form})
--- config/urls.py
+++ config/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 import app.views
 
 urlpatterns = [
-     path('hey/', app.views.hey_you, name='hey_you'),
-    path("", app.views.root, name="root"),
-    path("add/", app.views.add, name="add"),
+    path("hey/", app.views.hey_you, name="hey_you"),
+    path("age_in/", app.views.age_in, name="age_in"),
+    path("order_total/", app.views.order_total, name="order_total"),
     path("admin/", admin.site.urls),
 ]
